c6cf46dd5bbd52e4f6ac8bc3e83dca3b6763881e
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-02 21:38:33 +0530

    fix formatting and spacing in book object declaration

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let book={tittle:"The Great Gatsby",author:"F. Scott Fitzgerald",year:1925,
-    address:{city:"New York",country:"USA"}
+let book = {
+    tittle: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925,
+    address: { city: "New York", country: "USA" }
 };
-    console.log(Object.entries(book));
+console.log(Object.entries(book));
 
