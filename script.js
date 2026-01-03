3447ad74186da973c4aff72f5aefdc2066dd6b89
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-03 19:36:18 +0530

    refactor: remove book object and update script to log h1 element

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-let book = {
-    tittle: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925,
-    address: { city: "New York", country: "USA" }
-};
-console.log(Object.entries(book));
-
+let abc= document.querySelector("h1");
+console.log(abc);
