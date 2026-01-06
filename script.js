3f74a5d69114d83812929773ae5c727cf157423f
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-06 17:01:15 +0530

    refactor: update HTML structure and enhance JavaScript functionality for dynamic content

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 
+// get all ul elements
+let fruits = document.querySelectorAll("ul");
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i].innerText);
+}
 
-let h1= document.querySelector("h1").innerHTML="welcome to <i>js learning</i> Tutorial";
-document.querySelector("p").innerText="welcome to js learning Tutorial";
-let h2= document.createElement("h2")
-document.body.appendChild(h2);
-h2.textContent="welcome to <i>js learning</i> Tutorial";
-document.body.appendChild(h2);
-h2.innerHTML="welcome to <i>js learning</i> Tutorial";
 
-// Dark mode toggle button
-let toggleBtn = document.getElementById("toggleBtn");     
+
+
+//document.querySelector("p").innerHTML="<b>Upadated</b> by javascript";
+
+
+//get img src 
+
+let img = document.querySelector("img");
+// img.getAttribute("src");
+// console.log(img);
+
+//set img src
+img.setAttribute("src", "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d");
+img.style.width = "300px";
+img.style.height = "auto";
+img.style.borderRadius = "10px";
+
+
+//change  href link 
+document.querySelector("a").setAttribute("href", "https://vyankateshbairagi.netlify.app/")
+
+
+// add tiitle attribute to div 
+let div = document.createElement("div");
+div.innerText = "This is a div";
+document.body.append(div);
+div.setAttribute("title", "import info it is");
+
+
+//remove attribute 
+div.removeAttribute("title");
+
+//remove element
+document.body.removeChild(div);
+
+
+//create new list items <li> new task </li> and add to ul at the end
+
+let ul = document.createElement("ul");
+
+ul.innerHTML = "Group of task <li>add some feature</li> <li>add info feature</li><li>add login feature</li><li> new task </li> ";
+document.body.append(ul);
