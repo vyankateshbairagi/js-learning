73c2c4e043a137bd051a58f49c0763991a5d8918
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-07 19:54:49 +0530

    refactor: enhance HTML structure and add event listeners for form and input handling

--- Events and Event handling/script.js
+++ Events and Event handling/script.js
@@ -1,54 +1,76 @@
 
-//click event
-let h1 = document.querySelector("h1");
-h1.addEventListener("click",()=>{
-  console.log("clicked");
+// //click event
+// let h1 = document.querySelector("h1");
+// h1.addEventListener("click",()=>{
+//   console.log("clicked");
+// });
+
+// //remove event listener
+// let btn = document.querySelector("button");
+
+// function sayHi() {
+//   console.log("Hi");
+// }
+
+// btn.addEventListener("click", sayHi);
+// //btn.removeEventListener("click", sayHi);
+
+
+
+// //double click event
+// h1.addEventListener("dblclick",()=>{
+//     console.log("double clicked");
+// });
+
+// //mouse enter event
+// h1.addEventListener("mouseenter",()=>{
+//     console.log("mouse entered");
+// });
+
+// //mouse leave event
+// h1.addEventListener("mouseleave",()=>{
+//     console.log("mouse left");
+// });
+
+// //mouse over event
+// h1.addEventListener("mouseover",()=>{
+//     console.log("mouse over");
+// });
+
+// //load event
+// window.addEventListener("load",()=>{
+//     console.log("page loaded");
+// });
+
+
+// //scroll event
+// window.addEventListener("scroll",()=>{
+//     console.log("page scrolled");
+// }); 
+
+// //keydown event
+// window.addEventListener("keydown",(event)=>{
+//     console.log(`key ${event.key} pressed`);
+// });
+
+//input event
+let inp = document.querySelector("input");
+inp.addEventListener("input",(event)=>{
+    console.log(event.target.value); 
+  } );
+
+
+  //change event
+let city = document.querySelector("#citySelect");
+  city.addEventListener("change", () => {
+  console.log(city.value);
 });
 
-//remove event listener
-let btn = document.querySelector("button");
 
-function sayHi() {
-  console.log("Hi");
-}
+//submit event
+let form = document.querySelector("#myForm");
 
-btn.addEventListener("click", sayHi);
-//btn.removeEventListener("click", sayHi);
-
-
-
-//double click event
-h1.addEventListener("dblclick",()=>{
-    console.log("double clicked");
-});
-
-//mouse enter event
-h1.addEventListener("mouseenter",()=>{
-    console.log("mouse entered");
-});
-
-//mouse leave event
-h1.addEventListener("mouseleave",()=>{
-    console.log("mouse left");
-});
-
-//mouse over event
-h1.addEventListener("mouseover",()=>{
-    console.log("mouse over");
-});
-
-//load event
-window.addEventListener("load",()=>{
-    console.log("page loaded");
-});
-
-
-//scroll event
-window.addEventListener("scroll",()=>{
-    console.log("page scrolled");
-}); 
-
-//keydown event
-window.addEventListener("keydown",(event)=>{
-    console.log(`key ${event.key} pressed`);
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // page reload थांबवतो
+  console.log("Form submitted");
 });
