6e91edc8ce97b0d7aff280887d63d3b3b5926bb9
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-07 16:34:53 +0530

    refactor: clean up click event listener and improve code readability

--- Events and Event handling/script.js
+++ Events and Event handling/script.js
@@ -1,12 +1,9 @@
 
-
+//click event
 let h1 = document.querySelector("h1");
-// h1.addEventListener("click",()=>{
-//     console.log("clicked");
-// });
-
-
-
+h1.addEventListener("click",()=>{
+  console.log("clicked");
+});
 
 //remove event listener
 let btn = document.querySelector("button");
@@ -19,7 +16,28 @@ btn.addEventListener("click", sayHi);
 //btn.removeEventListener("click", sayHi);
 
 
+
+//double click event
 h1.addEventListener("dblclick",()=>{
     console.log("double clicked");
 });
 
+//mouse enter event
+h1.addEventListener("mouseenter",()=>{
+    console.log("mouse entered");
+});
+
+//mouse leave event
+h1.addEventListener("mouseleave",()=>{
+    console.log("mouse left");
+});
+
+//mouse over event
+h1.addEventListener("mouseover",()=>{
+    console.log("mouse over");
+});
+
+//load event
+window.addEventListener("load",()=>{
+    console.log("page loaded");
+});
