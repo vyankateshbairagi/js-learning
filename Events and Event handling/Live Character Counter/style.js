76bf7595f4d15dc619fd5987c2ab51b50e6192b5
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-09 19:43:48 +0530

    refactor: improve input handling and feedback for character and message inputs

--- Events and Event handling/Live Character Counter/style.js
+++ Events and Event handling/Live Character Counter/style.js
@@ -1,11 +1,40 @@
-let input = document.querySelector("input");
-let label = document.querySelector("label");
 
-input.addEventListener("input", () => {
-  if (input.value.length === 0) {
-    label.textContent = "Enter Your Character";
+let charInput = document.querySelector("#charInput");
+let charLabel = document.querySelector("#charLabel");
+
+charInput.addEventListener("input", () => {
+  if (charInput.value.length === 0) {
+    charLabel.textContent = "Enter Your Character";
   } else {
-    label.textContent = `You Enter ${input.value.length} Character`;
+    let abc = charInput.value.length;
+    charLabel.textContent = `You Enter ${abc} Character`;
   }
 });
+
+
+
+let msgInput = document.querySelector("#msgInput");
+let msgLabel = document.querySelector("#msgLabel");
+
+const maxLength = 100;
+
+msgInput.addEventListener("input", () => {
+  let length = msgInput.value.length;
+  let remaining = maxLength - length;
+
+  if (length === 0) {
+    msgLabel.textContent = "Enter Your Message";
+  } 
+  else if (remaining > 0) {
+    msgLabel.textContent = `You remain ${remaining} characters`;
+    msgLabel.style.color="black";
+
+  } 
+  else {
+    msgLabel.textContent = "You have reached the limit!";
+    msgLabel.style.color="red";
+    alert("Message limit reached!");
+  }
+});
+
 
