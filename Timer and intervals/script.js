5662e7676eeae606678689e8f4ccde4ecdb33d7a
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-01-11 20:05:56 +0530

    chore: comment out unused timer code for future reference

--- Timer and intervals/script.js
+++ Timer and intervals/script.js
@@ -1,12 +1,33 @@
 
-let count =5;
+// let count =5;
 
-let trvl = setInterval(() => {
+// let trvl = setInterval(() => {
 
-    if (count >= 0) {
-        console.log(count);
-        count--;
-    }else{
-    clearInterval(trvl);
-    }
-}, 1000)
+//     if (count >= 0) {
+//         console.log(count);
+//         count--;
+//     }else{
+//     clearInterval(trvl);
+//     }
+// }, 1000)
+
+
+document.getElementById('startTimeout').addEventListener('click', function() {
+    setTimeout(() => {
+        document.getElementById('output').innerText = "Timeout executed after 3 seconds!";
+    }, 3000);       
+});
+
+let intervalId; 
+document.getElementById('startInterval').addEventListener('click', function() {
+    intervalId = setInterval(() => {
+        const currentTime = new Date().toLocaleTimeString();
+        document.getElementById('output').innerText = "Current Time: " + currentTime;
+    }, 1000);
+});
+
+document.getElementById('stopInterval').addEventListener('click', function() {
+    clearInterval(intervalId);
+    document.getElementById('output').innerText = "Interval stopped.";
+}); 
+
