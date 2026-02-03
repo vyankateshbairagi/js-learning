e2bfcf48e0db5a92dfb62853ca6b4d4e3e51a089
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-02-03 18:25:33 +0530

    refactor: replace setTimeout calls with changeColor function for better color management

--- Advance JS/app.js
+++ Advance JS/app.js
@@ -1,17 +1,38 @@
 let h1 = document.querySelector("h1");
 
-setTimeout(()=>{
-    h1.style.color= "red";
-},1000);
 
-setTimeout(()=>{
-    h1.style.color= "cyan";
-},1000);
+// setTimeout(()=>{
+//     h1.style.color= "red";
+// },1000);
 
-setTimeout(()=>{
-    h1.style.color= "green";
-},1000);
+// setTimeout(()=>{
+//     h1.style.color= "cyan";
+// },2000);
 
-setTimeout(()=>{
-    h1.style.color= "orange";
-},1000);
+// setTimeout(()=>{
+//     h1.style.color= "green";
+// },3000);
+
+// setTimeout(()=>{
+//     h1.style.color= "orange";
+// },4000);
+
+
+
+
+// using callback 
+
+
+function changeColor(color,delay){
+    setTimeout(()=>{
+        h1.style.color=color;
+    }, delay);
+}
+
+changeColor("blue",1000);
+changeColor("red",2000);
+changeColor("yellow",3000);
+changeColor("cyan",4000);
+changeColor("pink",5000);
+changeColor("blue",6000);
+
