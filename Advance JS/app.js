caf55ea3cce3de09c36f12f0614c851fb8cd3009
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-02-03 18:10:32 +0530

    feat: add color-changing effects to h1 in app.js and ensure h1 is present in index.html

---
+++ Advance JS/app.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelector("h1");
+
+setTimeout(()=>{
+    h1.style.color= "red";
+},1000);
+
+setTimeout(()=>{
+    h1.style.color= "cyan";
+},1000);
+
+setTimeout(()=>{
+    h1.style.color= "green";
+},1000);
+
+setTimeout(()=>{
+    h1.style.color= "orange";
+},1000);
