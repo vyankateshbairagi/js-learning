6189a1cfa03c142ca982a7fa041a80c1a19fd746
Vyankatesh Bairagi <vyankateshbairagi@outlook.com>
2026-02-04 11:07:16 +0530

    fix: update colorChange function to handle promise resolution and rejection messages

--- Advance JS/app.js
+++ Advance JS/app.js
@@ -25,19 +25,60 @@ let h1 = document.querySelector("h1");
 // using callback 
 
 
-function changeColor(color, delay) {
-    setTimeout(() => {
-        h1.style.color = color;
-    }, delay);
+// function changeColor(color, delay) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//     }, delay);
+// }
+
+// changeColor("blue", 1000);
+// changeColor("red", 2000);
+// changeColor("yellow", 3000);
+// changeColor("cyan", 4000);
+// changeColor("pink", 5000);
+// changeColor("blue", 6000);
+
+
+
+//  color change using promise
+
+function colorChange(color, delay) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            h1.style.color = color;
+        }, delay);
+
+        if (color) {
+            res("color applied");
+        } else {
+            rej("color dont appied")
+        }
+
+    });
+
+
 }
 
-changeColor("blue", 1000);
-changeColor("red", 2000);
-changeColor("yellow", 3000);
-changeColor("cyan", 4000);
-changeColor("pink", 5000);
-changeColor("blue", 6000);
+colorChange("red", 2000).then(() => {
+    console.log(resolve);
+    return colorChange("green", 2000);
+})
 
+    .then(() => {
+        console.log(resolve);
+        return colorChange("red", 2000);
+    })
+    .then(() => {
+        console.log(resolve);
+        return colorChange("yellow", 2000);
+    })
+    .then(() => {
+        console.log(resolve);
+    })
+    .catch(() => {
+        console.log(err);
+
+    })
 
 
 
@@ -45,36 +86,36 @@ changeColor("blue", 6000);
 // promises chaining ..........
 
 
-function saveData(data) {
-    return new Promise((resolve, reject) => {
-        let internetSpeed = Math.floor(Math.random() * 10);
-        if (internetSpeed > 4) {
-            resolve(`Success with InternetSpeed of ${internetSpeed} mbps: ${data}  is saved`);
-        } else {
-            reject(`Failure with InternetSpeed of ${internetSpeed} mbps: ${data} not saved (Weak Connection)`);
-        }
+// function saveData(data) {
+//     return new Promise((resolve, reject) => {
+//         let internetSpeed = Math.floor(Math.random() * 10);
+//         if (internetSpeed > 4) {
+//             resolve(`Success with InternetSpeed of ${internetSpeed} mbps: ${data}  is saved`);
+//         } else {
+//             reject(`Failure with InternetSpeed of ${internetSpeed} mbps: ${data} not saved (Weak Connection)`);
+//         }
 
-    });
-}
-
-
-saveData("vyankatesh")
-    .then((res) => {
-        console.log(res);
-        return saveData("Gopaldas");
-    })
-    .then((res) => {
-        console.log(res);
-        return saveData("Bairagi");
-    })
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+//     });
+// }
 
 
-    
+// saveData("vyankatesh")
+//     .then((res) => {
+//         console.log(res);
+//         return saveData("Gopaldas");
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return saveData("Bairagi");
+//     })
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+
+
 
 
